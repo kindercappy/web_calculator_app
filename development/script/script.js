e839dd1dae67a06dbf11cef208ecab482446a45a
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-07-15 13:52:56 +0530

    all operations working for 2 numbers

--- development/script/script.js
+++ development/script/script.js
@@ -1,3 +1,61 @@
+function performCalc(oper,currVal,prevVal){
+	var intCurrVal = Number(currVal);
+	var intPrevVal = Number(prevVal);
+	var result = 0;
+	switch (oper) {
+		case '+':
+			result = intPrevVal + intCurrVal;
+			break;
+		case '-':
+			result = intPrevVal - intCurrVal;
+			break;
+		case 'x':
+			result = intPrevVal * intCurrVal;
+			break;
+		case '/':
+			result = intPrevVal / intCurrVal;
+			break;
+		default:
+			// statements_def
+			break;
+	}
+	return result;
+}
+
 $(document).ready(function(){
-	// alert('Yolo');
+	var currValue = 0;
+	var result = 0;
+	var collectNums = [];
+	var history = [];
+	var prevNo = 0;
+	var operator = '';
+	$('button').click(function(){
+		var currNo = 0;
+
+		currValue = $(this).attr('value');
+		// console.log(currValue);
+		if(currValue === '='){
+			currNo = collectNums.join("");
+			// console.log(currNo);
+			result = performCalc(operator,currNo,prevNo);
+			// prevNo = result;
+			$('.result').text(result);
+		}else if (currValue == '+' || currValue == '-' || currValue == 'x' || currValue == '/') {
+			operator = currValue;
+			console.log(operator);
+			prevNo = collectNums.join("");
+			collectNums = [];
+			// console.log(prevNo);
+			// console.log(collectNums);
+		}else{
+			collectNums.push(currValue);
+
+			// console.log(collectNums);
+		}
+		history.push(currValue);
+		$('.history').text(history.join(""));
+		// $('.collectNums').text(collectNums);
+		// $('.collectNums').text(collectNums);
+	});
+	
 });
