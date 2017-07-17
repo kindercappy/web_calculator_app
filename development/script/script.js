9781bacef7e1a344ba21c65a413a8080bbbe5344
Kinder Preet Singh <kp.cappy.kp@gmail.com>
2017-07-17 23:57:39 +0530

    can add 2 or more numbers by chaining, can add 2 numbers get the result and chain by pressing an operator or if a digit is pressed the calcs start again.

--- development/script/script.js
+++ development/script/script.js
@@ -30,38 +30,65 @@ function showHistory(history){
 $(document).ready(function(){
 	$("h5").fadeIn()
 	.css({bottom:1000,position:'absolute',left:5})
-	.animate({bottom:10}, 2000);
-	$('.calcButtonContainer').hide().fadeIn(4000);
+	.animate({bottom:0}, 2000);
+	$('.calcContainer').fadeIn(3000);
 	var currValue   = 0;	
 	var collectNums = [];
 	var history     = [];
 	var prevNo      = 0;
 	var operator    = '';
+	//pressing equal will set it to true. if a digit entered after that then it will set the result to 0 or else continue the operation
+	var checkIfDidgitEnteredAfterEqualPressed = false;
 	$('button').click(function(){
 		var currNo = 0;
 		var regex  = /^\d+$/;
+		var regexMatchDigit = /[0-9]/;
 		currValue  = $(this).attr('value');
+		if(currValue.match(regex) && checkIfDidgitEnteredAfterEqualPressed){
+			result=0;
+		}
 		if(currValue === '='){
-			//equals is pressed perform the operation\
+			//equals is pressed perform the operation
+				checkIfDidgitEnteredAfterEqualPressed = true;
 				if(collectNums.length > 0){
 					prevNo = collectNums.join("");
 					collectNums=[];
+					history=[];
 					result = performCalc(operator,prevNo);
-					var rounded =  result;
+					var rounded =  Math.round(result * 100)/100;
+					$('.history').text(rounded);
 					$('.result').text(rounded);
+					// result=0;
+					return;
 				}else {
+					$('.history').text(result);
 					$('.result').text(result);
+					// console.log(result);
+					return;
 				}
 		}//(currValue === '=')
 		else if (currValue == '+' || currValue == '-' || currValue == '*' || currValue == '/') {
+			if(currValue === '-' && collectNums.length <= 0){
+				if(history[history.length-1] !== currValue){
+					checkIfDidgitEnteredAfterEqualPressed=false;
+					history.push(currValue);
+					collectNums.push(currValue);
+					return;
+				}
+
+			}
 			if(history.length >= 0){
 				//get the firstNumber entered
-				if(result === 0 && history[0].match(regex)){
+				if(result === 0 ){
+					// console.log(collectNums.join(""));
 					result      = collectNums.join("");
+					checkIfDidgitEnteredAfterEqualPressed=false;
+					// console.log(result);
 					collectNums = [];
 				}else if(collectNums.length > 0){
 					//get the the next numbers
 					prevNo = collectNums.join("");
+					checkIfDidgitEnteredAfterEqualPressed=false;
 					result = performCalc(operator,prevNo);
 					collectNums = [];
 				}
@@ -73,7 +100,8 @@ $(document).ready(function(){
 			if(history[history.length-1] !== currValue && history[history.length-1] !== '+' && history[history.length-1] !== '-' && history[history.length-1] !== '/' && history[history.length-1] !== '*'){
 				operator = currValue;
 				history.push(currValue);
-				// return;
+				checkIfDidgitEnteredAfterEqualPressed=false;
+
 			}//(history[history.length-1] !== currValue && history[history.length-1] !== '+' && history[history.length-1] !== '-' && history[history.length-1] !== '/' && history[history.length-1] !== 'x')
 		}//(currValue == '+' || currValue == '-' || currValue == 'x' || currValue == '/')
 		else if(currValue === 'ce'){
